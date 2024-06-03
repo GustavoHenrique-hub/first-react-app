592c54828d499d957e3bcb786c475d13571978e1
Gustavo Henrique <gustavosilvah37@gmail.com>
2024-06-03 12:19:25 -0300

    feat: dashboard page finished

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -2,7 +2,6 @@ import "./dashboard.css";
 import { useOnKeyPress } from "../../hooks/useOnKeyPress";
 import { useState, useEffect } from "react";
 import TypeWeatherLogo from "../../assets/dashboardPage/btn-logo.svg";
-import MoonLogo from "../../assets/dashboardPage/moon-icon.svg";
 import Divider from "../../assets/dashboardPage/divider.svg";
 
 import Termometer from "../../assets/dashboardPage/detailIcons/termometer.svg";
@@ -10,6 +9,31 @@ import Rain from "../../assets/dashboardPage/detailIcons/rain.svg";
 import Wind from "../../assets/dashboardPage/detailIcons/wind.svg";
 import Drop from "../../assets/dashboardPage/detailIcons/drop.svg";
 import Sun from "../../assets/dashboardPage/detailIcons/sun.svg";
+
+import ClearDay from "../../assets/dashboardPage/weatherBackgrounds/clear-day.png";
+import ClearNight from "../../assets/dashboardPage/weatherBackgrounds/clear-night.png";
+import CloudyDay from "../../assets/dashboardPage/weatherBackgrounds/cloudy-day.png";
+import CloudyNight from "../../assets/dashboardPage/weatherBackgrounds/cloudy-night.png";
+import RainDay from "../../assets/dashboardPage/weatherBackgrounds/rain-day.png";
+import RainNight from "../../assets/dashboardPage/weatherBackgrounds/rain-night.png";
+import StormDay from "../../assets/dashboardPage/weatherBackgrounds/storm-day.png";
+import StormNight from "../../assets/dashboardPage/weatherBackgrounds/storm-night.png";
+import SnowDay from "../../assets/dashboardPage/weatherBackgrounds/snow-day.png";
+import SnowNight from "../../assets/dashboardPage/weatherBackgrounds/snow-night.png";
+import FewCloudsDay from "../../assets/dashboardPage/weatherBackgrounds/fewclouds-day.png";
+import FewCloudsNight from "../../assets/dashboardPage/weatherBackgrounds/fewclouds-night.png";
+
+import ClearDayIcon from "../../assets/dashboardPage/weatherIcons/clear-day.svg";
+import ClearNightIcon from "../../assets/dashboardPage/weatherIcons/clear-night.svg";
+import CloudyNightIcon from "../../assets/dashboardPage/weatherIcons/cloudy-night.svg";
+import FewCloudsDayIcon from "../../assets/dashboardPage/weatherIcons/fewclouds-day.svg";
+import FewCloudsNightIcon from "../../assets/dashboardPage/weatherIcons/fewclouds-night.svg";
+import RainDayIcon from "../../assets/dashboardPage/weatherIcons/rain-day.svg";
+import RainNightIcon from "../../assets/dashboardPage/weatherIcons/rain-night.svg";
+import SnowDayIcon from "../../assets/dashboardPage/weatherIcons/snow-day.svg";
+import SnowNightIcon from "../../assets/dashboardPage/weatherIcons/snow-night.svg";
+import StormDayIcon from "../../assets/dashboardPage/weatherIcons/storm-day.svg";
+import StormNightIcon from "../../assets/dashboardPage/weatherIcons/storm-night.svg";
 
 const key = "b2213bc688da17e335540298fee4f0b7";
 const detailKey = "f325bd9a5efd4399858165721242405";
@@ -44,45 +68,41 @@ function Dashboard() {
   const [stateTempMax, setStateTempMax] = useState();
   const [stateTempMin, setStateTempMin] = useState();
   const [stateWeather, setStateWeather] = useState("");
+
   const [stateCity, setStateCity] = useState("");
   const [stateTime, setStateTime] = useState("");
+
   const [stateChanceOfRain, setStateChanceOfRain] = useState();
   const [stateFeelsLike, setStateFeelsLike] = useState();
   const [stateHumidity, setStateHumidity] = useState();
   const [stateUvIndex, setStateUvIndex] = useState();
   const [stateWindKm, setStateWindKm] = useState();
+
   const [city, setCity] = useState("");
   const [country, setCountry] = useState("");
-  const [daysToShow, setDaysToShow] = useState(5);
+
+  const daysToShow = 5;
   const [forecastArray, setForecastArray] = useState([]);
 
-  const clearFields = () => {
-    setStateTemperatura();
-    setStateTempMax();
-    setStateTempMin();
-    setStateWeather("");
-    setStateFeelsLike();
-    setStateChanceOfRain();
-    setStateHumidity();
-    setStateWindKm();
-    setStateUvIndex();
-    setCountry("");
-    setCity("")
-  };
+  const [backgroundImage, setBackgroundImage] = useState(ClearDay);
+  const [iconsCondition, setIconsCondition] = useState(ClearDayIcon);
+  let [weatherCondition, setWeatherCondition] = useState();
 
   const callApi = () => {
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${stateCity}&lang=pt_br&appid=${key}&units=metric`;
-    const geocoderUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${stateCity}&lang=pt_br&appid=${key}`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${stateCity}&appid=${key}&units=metric`;
+    const geocoderUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${stateCity}&appid=${key}`;
     const weatherDetailsUrl = `http://api.weatherapi.com/v1/forecast.json?key=${detailKey}&q=${stateCity}`;
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${stateCity}&lang=pt_br&appid=${key}&units=metric`;
-
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${stateCity}&appid=${key}&units=metric`;
     fetch(weatherUrl)
       .then((response) => response.json())
       .then((dadoTemperatura) => {
+        console.log(dadoTemperatura);
         setStateTemperatura(dadoTemperatura.main.temp);
         setStateTempMax(dadoTemperatura.main.temp_max);
         setStateTempMin(dadoTemperatura.main.temp_min);
         setStateWeather(dadoTemperatura.weather[0].main);
+        setWeatherCondition(dadoTemperatura.weather[0].description);
+        console.log(weatherCondition);
       })
       .catch((error) => console.error("Error fetching weather data:", error));
 
@@ -135,15 +155,68 @@ function Dashboard() {
     months[now.getUTCMonth()]
   } ${now.getUTCFullYear()}`;
 
+  useEffect(() => {
+    const hoursNow = new Date();
+    if (weatherCondition === "clear sky" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(ClearDay);
+      setIconsCondition(ClearDayIcon);
+    } else if (weatherCondition === "clear sky" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(ClearNight);
+      setIconsCondition(ClearNightIcon);
+    } else if (weatherCondition === "few clouds" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(FewCloudsDay);
+      setIconsCondition(FewCloudsDayIcon);
+    } else if (weatherCondition === "few clouds" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(FewCloudsNight);
+      setIconsCondition(FewCloudsNightIcon);
+    } else if (weatherCondition === "mist" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(CloudyDay);
+      setIconsCondition(CloudyNightIcon);
+    } else if (weatherCondition === "mist" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(CloudyNight);
+      setIconsCondition(CloudyNightIcon);
+    } else if (weatherCondition === "broken clouds" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(CloudyDay);
+      setIconsCondition(CloudyNightIcon);
+    } else if (weatherCondition === "broken clouds" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(CloudyNight);
+      setIconsCondition(CloudyNightIcon);
+    } else if (weatherCondition === "rain" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(RainDay);
+      setIconsCondition(RainDayIcon);
+    } else if (weatherCondition === "rain" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(RainNight);
+      setIconsCondition(RainNightIcon);
+    } else if (weatherCondition === "shower rain" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(RainDay);
+      setIconsCondition(RainDayIcon);
+    } else if (weatherCondition === "shower rain" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(RainNight);
+      setIconsCondition(RainNightIcon);
+    } else if (weatherCondition === "thunderstorm" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(StormDay);
+      setIconsCondition(StormDayIcon);
+    } else if (weatherCondition === "thunderstorm" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(StormNight);
+      setIconsCondition(StormNightIcon);
+    } else if (weatherCondition === "snow" && hoursNow.getHours() <= 12) {
+      setBackgroundImage(SnowDay);
+      setIconsCondition(SnowDayIcon);
+    } else if (weatherCondition === "snow" && hoursNow.getHours() >= 13) {
+      setBackgroundImage(SnowNight);
+      setIconsCondition(SnowNightIcon);
+    }
+  }, [weatherCondition]);
+
   const getWeatherForNextDays = (daysToShow) => {
-    const todayIndex = now.getUTCDay();
+    const todayIndex = now.getUTCDay() + 1;
     const nextDays = [];
-  
-    for (let i = 1; i <= daysToShow; i++) {
+
+    for (let i = 0; i < daysToShow; i++) {
       const nextDayIndex = (todayIndex + i) % 7;
       nextDays.push(days[nextDayIndex]);
     }
-  
+
     return nextDays;
   };
 
@@ -190,13 +263,15 @@ function Dashboard() {
               value={stateCity}
               onChange={(event) => {
                 setStateCity(event.target.value);
-                clearFields();
               }}
             />
           </div>
 
           {/* Container de resposta da api */}
-          <div className="responseContainer">
+          <div
+            className="responseContainer"
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+          >
             {/* Header do container de resposta da api */}
             <div className="responseHeader">
               <p className="responseTitle">
@@ -218,25 +293,38 @@ function Dashboard() {
                     : " "}
                 </p>
                 <p className="responseFooterSubtitle">
-                  {!Number.isNaN(parseInt(stateTempMax)) && !Number.isNaN(parseInt(stateTempMin))
+                  {!Number.isNaN(parseInt(stateTempMax)) &&
+                  !Number.isNaN(parseInt(stateTempMin))
                     ? `${parseInt(stateTempMax)}°c / ${parseInt(
                         stateTempMin
                       )}°c`
                     : " "}
-                  {!Number.isNaN(parseInt(stateTemperatura)) ?
-                    (<img
+                  {!Number.isNaN(parseInt(stateTemperatura)) ? (
+                    <img
                       className="responseTextDivider"
                       src={Divider}
                       alt="Divider"
-                    /> ): " "
-                  }
+                    />
+                  ) : (
+                    " "
+                  )}
                   <span className="responseTextWeather">
                     {city && stateWeather ? ` ${stateWeather}` : " "}
                   </span>
                 </p>
               </div>
               <div className="responseFooterRight">
-                <img src={MoonLogo} alt="Moon Logo" />
+                <div className="responseFooterImage">
+                {iconsCondition !== undefined ? (
+                  <img src={iconsCondition} alt="Weather Logo" />
+                ) : (
+                  <img
+                    src="https://placehold.co/250x250"
+                    className="imagePlaceholder"
+                    alt="Weather Logo"
+                  />
+                )}
+                </div>
               </div>
             </div>
           </div>
@@ -314,7 +402,9 @@ function Dashboard() {
             </div>
             <div className="infoDetailsInRight">
               <p className="textRight">
-                {!Number.isNaN(parseInt(stateHumidity))  ? `${stateHumidity}%` : " - "}
+                {!Number.isNaN(parseInt(stateHumidity))
+                  ? `${stateHumidity}%`
+                  : " - "}
               </p>
             </div>
           </div>
@@ -331,7 +421,9 @@ function Dashboard() {
             </div>
             <div className="infoDetailsInRight">
               <p className="textRight">
-                {!Number.isNaN(parseInt(stateUvIndex)) ? `${stateUvIndex}` : " - "}
+                {!Number.isNaN(parseInt(stateUvIndex))
+                  ? `${stateUvIndex}`
+                  : " - "}
               </p>
             </div>
           </div>
@@ -347,10 +439,15 @@ function Dashboard() {
               <div key={index} className="forecastContainerDays">
                 <p className="forecastContainerDaysTitle">{day}</p>
                 <div className="forecastContainerImage">
-                  {forecastArray[index] ? 
-                    (<img src={forecastArray[index].icon} alt="Weather Icon" />) :
-                    (<img src="https://placehold.co/56x56" className="imagePlaceholder" alt="Weather Icon" />)
-                  }
+                  {forecastArray[index] ? (
+                    <img src={forecastArray[index].icon} alt="Weather Icon" />
+                  ) : (
+                    <img
+                      src="https://placehold.co/56x56"
+                      className="imagePlaceholder"
+                      alt="Weather Icon"
+                    />
+                  )}
                 </div>
                 <p className="forecastContainerDaysTitle">
                   {forecastArray[index] && forecastArray[index].main
